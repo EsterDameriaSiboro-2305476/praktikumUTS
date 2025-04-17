036862f493dd2670084b94da295b0ca861497106
Ester Dameria Siboro <ester06siboro@gmail.com>
2025-04-17 11:25:50 +0700

    MainBalok.java

--- MainBalok.java
+++ MainBalok.java
@@ -11,12 +11,27 @@ package ester2305476.utspraktikum;
 public class MainBalok {
    public static void main(String[] args) {
         balok b1 = new balok(1, 1, 1);
-        b1.cetakInfo();
+        System.out.println("Balok dengan panjang: " + b1.getPanjang()
+                + ", lebar: " + b1.getLebar()
+                + " dan tinggi: " + b1.getTinggi()
+                + ". Luasnya: " + b1.getLuas()
+                + ", kelilingnya: " + b1.getKeliling()
+                + " dan volumenya: " + b1.getVolume());
 
         balok b2 = new balok(30, 40, 50);
-        b2.cetakInfo();
+        System.out.println("Balok dengan panjang: " + b2.getPanjang()
+                + ", lebar: " + b2.getLebar()
+                + " dan tinggi: " + b2.getTinggi()
+                + ". Luasnya: " + b2.getLuas()
+                + ", kelilingnya: " + b2.getKeliling()
+                + " dan volumenya: " + b2.getVolume());
 
         balok b3 = new balok(25, 35, 45);
-        b3.cetakInfo();
+        System.out.println("Balok dengan panjang: " + b3.getPanjang()
+                + ", lebar: " + b3.getLebar()
+                + " dan tinggi: " + b3.getTinggi()
+                + ". Luasnya: " + b3.getLuas()
+                + ", kelilingnya: " + b3.getKeliling()
+                + " dan volumenya: " + b3.getVolume());
     }
 }
